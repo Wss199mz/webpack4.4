3c50628147e809e1a514e4ea21a6b9fbe65da249
wangshanshan <wangshanshan@zto.cn>
2020-03-04 23:12:45 +0800

    eslint

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # webpack4.4
-webpack4.4打包配置
+# webpack4.4打包配置
+
+### 一、安装依赖：npm i install
+
+### 二、启动项目：
+### 两种启动方式: （不同的方式需要entry不同, 这两种都配置了跨域代理）
+##### 1.通过 webpack-dev-server 开启服务 ->> npm run start
+##### 1.通过 express 开启服务 ->> npm run server
+
+### 三、项目打包：npm run build
--- build/dev-server.js
+++ build/dev-server.js
@@ -2,9 +2,12 @@ const webpackDevServer = require('webpack-dev-server');
 const webpack = require('webpack');
 
 const config = require('./webpack.dev.js');
+const devServer = require('./dev');
 const options = {
   contentBase: './dist',
   hot: true,
+  overlay: true,
+  quiet: true,
   host: 'localhost'
 };
 
@@ -12,6 +15,6 @@ webpackDevServer.addDevServerEntrypoints(config, options);
 const compiler = webpack(config);
 const server = new webpackDevServer(compiler, options);
 
-server.listen(5000, 'localhost', () => {
+server.listen(devServer.port, 'localhost', () => {
   console.log('dev server listening on port 5000');
 });
